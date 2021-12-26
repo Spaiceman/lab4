b899f64945f9b022edf9e019bfae588f4a741953
Spaiceman <sasha.moskalev00@mail.ru>
2021-12-26 14:55:40 +0300

    feat: add setting files

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+import directorRouter from './resources/directors/director.router';
+import filmRouter from './resources/films/film.router';
+import resultRouter from './resources/results/result.router';
+
+import { notFound, successHttpLogger, errorHttpLogger, errorLogger } from './middlewares';
+
+const app = express();
+app.use(express.json());
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  if (req.originalUrl === '/') {
+    res.send('Service is running!');
+    return;
+  }
+  next();
+});
+
+
+app.use(successHttpLogger);
+app.use(errorHttpLogger);
+
+app.use('/directors', directorRouter);
+app.use('/films', filmRouter);
+app.use('/results', resultRouter);
+
+app.use(notFound);
+app.use(errorLogger);
+
+
+export default app;
